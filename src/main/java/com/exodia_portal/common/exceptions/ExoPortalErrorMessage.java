29d5b17bcda9231a924461f48bd7ce5116ea966a
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-01 23:22:58 +0800

    feat: add logging to exception handler and refactor errorType handling for improved troubleshooting and type safety

--- src/main/java/com/exodia_portal/common/exceptions/ExoPortalErrorMessage.java
+++ src/main/java/com/exodia_portal/common/exceptions/ExoPortalErrorMessage.java
@@ -1,32 +1,24 @@
 package com.exodia_portal.common.exceptions;
 
 import com.exodia_portal.common.constant.ExoErrorTypeEnum;
+import lombok.Getter;
 
 import java.util.List;
 import java.util.Map;
 
+@Getter
 public class ExoPortalErrorMessage extends RuntimeException {
-    private int status;
-    private String errorType;
-    private List<Map<String, String>> errorMessageList;
+    private final int status;
+
+    private final ExoErrorTypeEnum errorType;
+
+    private final List<Map<String, String>> errorMessageList;
 
 
     public ExoPortalErrorMessage(int status, ExoErrorTypeEnum errorType, List<Map<String, String>> errorMessageList) {
         super(String.format("Error occurred with status: %d, type: %s", status, errorType));
         this.status = status;
-        this.errorType = errorType.toString();
+        this.errorType = errorType;
         this.errorMessageList = errorMessageList;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public String getErrorType() {
-        return errorType;
-    }
-
-    public List<Map<String, String>> getErrorMessageList() {
-        return errorMessageList;
     }
 }
