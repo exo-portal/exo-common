52ea26d268c4fc6d4232daa85c6ca3c70c528ec7
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-02 01:24:55 +0800

    Refactor: Replace hard-coded map keys with constants for "fieldName" and "errorMessage" to enhance maintainability

--- src/main/java/com/exodia_portal/common/constant/ExoConstant.java
+++ src/main/java/com/exodia_portal/common/constant/ExoConstant.java
@@ -9,4 +9,14 @@ public class ExoConstant {
     public static final String EXO_JSESSION_ID = "JSESSIONID";
 
     public static final String IS_LOGGED_IN = "isLoggedIn";
+
+    public static final String FIELD_NAME_KEY = "fieldName";
+
+    public static final String ERROR_MESSAGE_KEY = "errorMessage";
+
+    public static final String STATUS_KEY = "status";
+
+    public static final String ERROR_TYPE_KEY = "errorType";
+
+    public static final String ERROR_MESSAGE_LIST_KEY = "errorMessageList";
 }
