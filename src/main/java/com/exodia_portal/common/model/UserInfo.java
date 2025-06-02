94e04142b2b5de01761ca5fadd0158a54fdda171
Gene Paul Mar Javier <77821882+javiergenepaul@users.noreply.github.com>
2025-06-02 20:20:35 +0800

    docs: Add class-level Javadoc for ExoErrorUtil and explicitly define @JoinColumn for user relationship in UserInfo

--- src/main/java/com/exodia_portal/common/model/UserInfo.java
+++ src/main/java/com/exodia_portal/common/model/UserInfo.java
@@ -6,6 +6,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -55,6 +56,7 @@ public class UserInfo extends Auditable {
 
     @JsonBackReference
     @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
 }
